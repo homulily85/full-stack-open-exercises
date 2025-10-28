445d958ab46a0968176920397aad60d6df492e68
homulily85 <157974079+homulily85@users.noreply.github.com>
2025-10-28 11:35:24 +0700

    Finish exercises 7.4-7.6

--- part7/routed-anecdotes/src/component/CreateNew.jsx
+++ part7/routed-anecdotes/src/component/CreateNew.jsx
@@ -1,22 +1,33 @@
-import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useField } from '../hooks/index.js'
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
+
+  const { reset: resetContent, ...contentProps } = content
+  const { reset: resetAuthor, ...authorProps } = author
+  const { reset: resetInfo, ...infoProps } = info
+
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     })
 
     navigate('/')
+  }
+
+  const resetFields = () => {
+    resetContent()
+    resetAuthor()
+    resetInfo()
   }
 
   return (
@@ -25,20 +36,18 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name="content" value={content}
-                 onChange={(e) => setContent(e.target.value)}/>
+          <input name="content" {...contentProps}/>
         </div>
         <div>
           author
-          <input name="author" value={author}
-                 onChange={(e) => setAuthor(e.target.value)}/>
+          <input name="author" {...authorProps}/>
         </div>
         <div>
           url for more info
-          <input name="info" value={info}
-                 onChange={(e) => setInfo(e.target.value)}/>
+          <input name="info" {...infoProps}/>
         </div>
         <button>create</button>
+        <button type="button" onClick={resetFields}>reset</button>
       </form>
     </div>
   )
